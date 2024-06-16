e2697e5fc77ba061b59555d8c294f7e3b460e04e
cs <cs.ia1387@gmail.com>
2024-06-16 22:06:40 +1000

    front start

--- portone/app/page.tsx
+++ portone/app/page.tsx
@@ -1,24 +1,34 @@
 import Image from "next/image";
 const items: [string, string, string, string, string] = [
   "Programming",
-  "Computers",
   "Engineering",
-  "Websites",
-  "Technology",
+  "Web Dev",
+  "Computing",
+  "Something",
 ];
 export default function Home() {
   return (
     <>
-      <section className="flex flex-col md:flex-row h-200 bg-ayellowl order-1">
-        <div className="w-100% md:w-50% h-auto  p-5">
+      <section className="flex flex-col lg:flex-row h-front-first-h bg-gr-front-one order-1">
+        <div className="hidden w-0 h-0 lg:w-50% lg:h-front-first p-5 bg-red-900">
           {/* <img src="/background-front-1.svg" /> */}
-          <div className="bg-black w-100% h-100%"></div>
+          <div className="bg-black lg:w-100% lg:h-100%"></div>
         </div>
-        <div className="w-100% md:w-50% h-auto pr-5 pt-16" dir="rtl">
-          <ul className="text-right w-100% font-inc">
+        <div className="w-100% lg:w-50% h-auto sm:pr-5">
+          <div className="p-10 text-left">
+            <h2 className="text-5xl mb-2">
+              Fields of{" "}
+              <span className="text-abluelight underlinemaybe">interest</span>
+            </h2>
+            <p>Click on each to learn more!👇</p>
+          </div>
+          <ul className="text-right w-100% font-jb" dir="rtl">
             {items.map((item) => (
-              <li key={item} className="flex flex-row m-10">
-                <span>
+              <button
+                key={item}
+                className="flex flex-row m-5 w-90% sm:m-10 pr-4 p-10 sm:pr-10 bg-gray-300 bg-opacity-50 sm:w-110 YES  border-neutral-400 border-2 text-neutral-600 hover:text-white hover:shadow-[inset_-36rem_0_0_0] hover:shadow-ayellowlighter duration-[400ms,700ms] transition-[color,box-shadow]"
+              >
+                <span className="max-first">
                   <img
                     src="/bulletin1.svg"
                     alt="bulletin for list"
@@ -27,10 +37,9 @@ export default function Home() {
                   />
                 </span>
                 <span className="text-4xl mr-5">{item}</span>
-              </li>
+              </button>
             ))}
           </ul>
-          <div className="h-[1000px]"></div>
         </div>
       </section>
     </>
--- portone/tailwind.config.ts
+++ portone/tailwind.config.ts
@@ -13,19 +13,24 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
         front: "url('/front-maybe.bmp')",
+        "gr-front-one":
+          "linear-gradient(0deg, rgba(255,255,255,0) 0%, rgba(200,225,251,1) 10%, rgba(221,237,253,1) 100%)",
       },
       colors: {
         abluelight: "#2a4a6b",
         abluedark: "#031d38",
         ayellow: "#d49428",
         ayellowl: "#d4a928",
+        ayellowlighter: "#e3c15d",
         ayellowdark: "#ab7315",
       },
       spacing: {
         "110": "30rem",
         "200": "50rem",
+        "front-first-h": "70rem",
         "50%": "50%",
         "100%": "100%",
+        "90%": "90%",
       },
     },
   },
